67ec5a492a22e4abd3697296844fce83c890eed2
Nguyen Quang Minh <nguyenquangminh570@gmaiil.com>
2024-08-28 10:44:31 +0700

    feat: create getAllTasks, createTask

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { TaskModel } from './task.model';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): TaskModel[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): TaskModel {
+    return this.tasksService.createTask(title, description);
+  }
+}
